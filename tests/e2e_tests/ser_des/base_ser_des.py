88139b3a5f15f5307ded209aa8a85821d9f749c3
Karolina-Bogacka <kbogacka766@gmail.com>
2025-05-14 09:33:51 +0200

    Add integration tests (#93)
    ---------
    
    Co-authored-by: Karolina Bogacka <karolina@neverblink.eu>
    Co-authored-by: Bartosz Sławecki <bartosz@neverblink.eu>

---
+++ tests/e2e_tests/ser_des/base_ser_des.py
@@ -0,0 +1,167 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+from rdflib import Dataset, Graph
+
+from pyjelly.options import StreamOptions
+
+TripleGraphType = Union[Graph]
+QuadGraphType = Union[Dataset]
+
+
+class BaseSerDes(ABC):
+    """
+    Base class for serialization and deserialization tests.
+
+    This class provides a common interface for reading and writing
+    jelly/nquads/ntriples from and to byte streams. It is intended to be subclassed
+    for specific libraries used for serialization and deserialization.
+
+    Attributes:
+        name (str): The name of the serialization/deserialization library.
+
+    """
+
+    name: str
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @abstractmethod
+    def len_quads(self, graph: QuadGraphType) -> int:
+        """
+        Get the number of quads in the graph-like structure.
+
+        Args:
+            graph (QuadGraphType): The graph-like structure containing the quads.
+
+        Returns:
+            int: The number of quads in the graph.
+
+        """
+
+    @abstractmethod
+    def len_triples(self, graph: TripleGraphType) -> int:
+        """
+        Get the number of triples in the graph-like structure.
+
+        Args:
+            graph (TripleGraphType): The graph-like structure containing the triples.
+
+        Returns:
+            int: The number of triples in the graph.
+
+        """
+
+    @abstractmethod
+    def read_quads(self, in_bytes: bytes) -> QuadGraphType:
+        """
+        Read quads from bytes to a graph-like structure.
+
+        Args:
+            in_bytes (bytes): The input bytes to read from.
+
+        Returns:
+            QuadGraphType: The graph-like structure containing the quads.
+
+        """
+
+    @abstractmethod
+    def write_quads(self, in_graph: QuadGraphType) -> bytes:
+        """
+        Write serialized quads to bytes.
+
+        Args:
+            in_graph (QuadGraphType): The graph-like structure containing the quads.
+
+        Returns:
+            bytes: The output bytes containing the serialized quads.
+
+        """
+
+    @abstractmethod
+    def read_quads_jelly(self, in_bytes: bytes) -> QuadGraphType:
+        """
+        Read quads from jelly bytes.
+
+        Args:
+            in_bytes (bytes): The input bytes to read from.
+
+        Returns:
+            QuadGraphType: The graph-like structure containing the quads.
+
+        """
+
+    @abstractmethod
+    def write_quads_jelly(
+        self, in_graph: QuadGraphType, options: StreamOptions, frame_size: int
+    ) -> bytes:
+        """
+        Write quads to jelly bytes.
+
+        Args:
+            in_graph (QuadGraphType): The graph-like structure containing the quads.
+            options (StreamOptions): The stream options for serialization.
+            frame_size (int): The size of the frame for serialization.
+
+        Returns:
+            bytes: The output bytes containing the serialized quads.
+
+        """
+
+    @abstractmethod
+    def read_triples(self, in_bytes: bytes) -> TripleGraphType:
+        """
+        Read triples from bytes to a graph-like structure.
+
+        Args:
+            in_bytes (bytes): The input bytes to read from.
+
+        Returns:
+            TripleGraphType: The graph-like structure containing the triples.
+
+        """
+
+    @abstractmethod
+    def write_triples(self, in_graph: TripleGraphType) -> bytes:
+        """
+        Write triples to bytes.
+
+        Args:
+            in_graph (TripleGraphType): The graph-like structure containing the triples.
+
+        Returns:
+            bytes: The output bytes containing the serialized triples.
+
+        """
+
+    @abstractmethod
+    def read_triples_jelly(self, in_bytes: bytes) -> TripleGraphType:
+        """
+
+        Read triples from jelly bytes.
+
+        Args:
+            in_bytes (bytes): The input bytes to read from.
+
+        Returns:
+            TripleGraphType: The graph-like structure containing the triples.
+
+        """
+
+    @abstractmethod
+    def write_triples_jelly(
+        self, in_graph: TripleGraphType, options: StreamOptions, frame_size: int
+    ) -> bytes:
+        """
+        Write triples to jelly bytes.
+
+        Args:
+            in_graph (TripleGraphType): The graph-like structure containing the triples.
+            options (StreamOptions): The stream options for serialization.
+            frame_size (int): The size of the frame for serialization.
+
+        Returns:
+            bytes: The output bytes containing the serialized triples.
+
+        """
---
+++ tests/e2e_tests/test_e2e.py
@@ -0,0 +1,95 @@
+from dataclasses import replace
+from itertools import chain, product
+from pathlib import Path
+
+import pytest
+
+from pyjelly.options import StreamOptions
+from tests.e2e_tests.ser_des.base_ser_des import BaseSerDes
+from tests.e2e_tests.ser_des.rdflib_ser_des import RdflibSerDes
+
+
+class End2EndOptionSetup:
+    """Set up stream options, file size and file name for E2E tests."""
+
+    test_root: Path = Path("tests/e2e_tests/test_cases/")
+
+    def setup_ser_des(self) -> list[tuple[BaseSerDes, BaseSerDes, StreamOptions, int]]:
+        """Set up test serializer, deserializer, options and frame_size."""
+        ser = [RdflibSerDes()]
+        des = [RdflibSerDes()]
+        # We want to have a variety of options to test
+        # Particularly examples of small lookup sizes
+        # and a lack of prefix
+        small_no_pref = replace(StreamOptions.small(), prefix_lookup_size=0)
+        tiny_tiny_pred = replace(
+            StreamOptions.small(), name_lookup_size=16, prefix_lookup_size=8
+        )
+        options = [
+            StreamOptions.small(),
+            small_no_pref,
+            tiny_tiny_pred,
+            StreamOptions.big(),
+        ]
+        file_sizes = [1, 4, 200, 10_000]
+        return list(product(ser, des, options, file_sizes))
+
+    def setup_triple_files(
+        self,
+    ) -> list[tuple[BaseSerDes, BaseSerDes, StreamOptions, int, Path]]:
+        """Set up options for each of the test triple files."""
+        test_dir: Path = self.test_root / "triples_rdf_1_1"
+        files = test_dir.glob("*.nt")
+        options = self.setup_ser_des()
+        return list(chain(*[[(*o, f) for o in options] for f in files]))
+
+    def setup_quad_files(
+        self,
+    ) -> list[tuple[BaseSerDes, BaseSerDes, StreamOptions, int, Path]]:
+        """Set up options for each of the test quad files."""
+        test_dir: Path = self.test_root / "quads_rdf_1_1"
+        files = test_dir.glob("*.nq")
+        options = self.setup_ser_des()
+        return list(chain(*[[(*o, f) for o in options] for f in files]))
+
+
+class TestEnd2End:
+    setup = End2EndOptionSetup()
+
+    @pytest.mark.parametrize(
+        ("ser", "des", "options", "frame_size", "file"), setup.setup_triple_files()
+    )
+    def test_triple_files(
+        self,
+        ser: BaseSerDes,
+        des: BaseSerDes,
+        options: StreamOptions,
+        frame_size: int,
+        file: Path,
+    ) -> None:
+        nt_reader = RdflibSerDes()
+        with file.open("rb") as f:
+            if (in_file := f) is not None:
+                triples = nt_reader.read_triples(in_file.read())
+                jelly_io = ser.write_triples_jelly(triples, options, frame_size)
+                new_g = des.read_triples_jelly(jelly_io)
+                assert nt_reader.len_triples(triples) == des.len_triples(new_g)
+
+    @pytest.mark.parametrize(
+        ("ser", "des", "options", "frame_size", "file"), setup.setup_quad_files()
+    )
+    def test_quad_files(
+        self,
+        ser: BaseSerDes,
+        des: BaseSerDes,
+        options: StreamOptions,
+        frame_size: int,
+        file: Path,
+    ) -> None:
+        nq_reader = RdflibSerDes()
+        with file.open("rb") as f:
+            if (in_file := f) is not None:
+                quads = nq_reader.read_quads(in_file.read())
+                jelly_io = ser.write_quads_jelly(quads, options, frame_size)
+                new_g = des.read_quads_jelly(jelly_io)
+                assert nq_reader.len_quads(quads) == des.len_quads(new_g)
