18dda7a17e81f49d9ef9babd5c9afbf584bc5119
bswck <bartosz@neverblink.eu>
2025-05-15 20:37:24 +0200

    Workaround 2

--- tests/utils/rdflib_workarounds.py
+++ tests/utils/rdflib_workarounds.py
@@ -16,8 +16,14 @@ def fixup_term(term: rdflib.Node) -> None:
     True
     """
     # workaround #99
-    if isinstance(term, rdflib.Literal) and term.datatype == rdflib.XSD.string:
-        term._datatype = None
+    if isinstance(term, rdflib.Literal):
+        if term.datatype == rdflib.XSD.string:
+            term._datatype = None
+        # workaround #101
+        if term.datatype == rdflib.XSD.integer:
+            term._value = "0"
+        if term.datatype == rdflib.XSD.decimal:
+            term._value = "0.0"
 
 
 def fixup_graph(graph: rdflib.Graph) -> None:
