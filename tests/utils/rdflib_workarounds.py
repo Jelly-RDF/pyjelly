a4021a56b0a9c1d019687a4008499d61d9a11bd2
bswck <bartosz@neverblink.eu>
2025-05-15 17:32:11 +0200

    Add extra test

--- tests/utils/rdflib_workarounds.py
+++ tests/utils/rdflib_workarounds.py
@@ -1,9 +1,20 @@
+# ruff: noqa: D205,D400,D415
 from __future__ import annotations
 
 import rdflib
 
 
 def fixup_term(term: rdflib.Node) -> None:
+    """
+    >>> l1 = rdflib.Literal("foo", datatype=rdflib.XSD.string)
+    >>> l2 = rdflib.Literal("foo")
+    >>> l1 == l2
+    False
+    >>> fixup_term(l1)
+    >>> fixup_term(l2)
+    >>> l1 == l2
+    True
+    """
     # workaround #99
     if isinstance(term, rdflib.Literal) and term.datatype == rdflib.XSD.string:
         term._datatype = None
