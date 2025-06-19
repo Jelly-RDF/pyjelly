7e655853b12ee0ebf803a0e42f19e3129f2e4160
Anastasiya Danilenka <anastasiya.danilenka.dokt@pw.edu.pl>
2025-06-19 10:36:41 +0200

    fix type check issues and so on

--- pyjelly/integrations/rdflib/parse.py
+++ pyjelly/integrations/rdflib/parse.py
@@ -85,15 +85,13 @@ class RDFLibTriplesAdapter(RDFLibAdapter):
         triples and namespaces and can get flushed between frames.
     """
 
-    graph: Graph
-
     def __init__(self, options: ParserOptions, graph: StatementSink) -> None:
         super().__init__(options=options)
         self.graph = graph
 
     @override
     def triple(self, terms: Iterable[Any]) -> Any:
-        self.graph.add(terms)  # type: ignore[arg-type]
+        self.graph.add(terms)
 
     @override
     def namespace_declaration(self, name: str, iri: str) -> None:
@@ -150,7 +148,7 @@ class RDFLibQuadsAdapter(RDFLibQuadsBaseAdapter):
 
     @override
     def quad(self, terms: Iterable[Any]) -> Any:
-        self.dataset.add(terms)  # type: ignore[arg-type]
+        self.dataset.add(terms)
 
 
 class RDFLibGraphsAdapter(RDFLibQuadsBaseAdapter):
@@ -217,7 +215,7 @@ def parse_flat_triples_stream(
     frames: Iterable[jelly.RdfStreamFrame],
     options: ParserOptions,
     graph: StatementSink,
-) -> Graph:
+) -> StatementSink:
     """
     Parse flat triple stream.
 
@@ -242,7 +240,7 @@ def parse_flat_quads_stream(
     frames: Iterable[jelly.RdfStreamFrame],
     options: ParserOptions,
     dataset: StatementSink,
-) -> Dataset:
+) -> StatementSink:
     """
     Parse flat quads stream.
 
@@ -271,7 +269,7 @@ def parse_graph_stream(
     frames: Iterable[jelly.RdfStreamFrame],
     options: ParserOptions,
     dataset: StatementSink,
-) -> Generator[Graph]:
+) -> Generator[StatementSink]:
     """
     Parse graph stream.
 
@@ -284,7 +282,8 @@ def parse_graph_stream(
         Generator[Graph]: returns one graph per frame
 
     """
-    adapter = RDFLibTriplesAdapter(options, dataset=dataset)
+    # TODO (Nastya): this should be adapted further
+    adapter = RDFLibTriplesAdapter(options, graph=dataset)
     decoder = Decoder(adapter=adapter)
     for frame in frames:
         yield decoder.decode_frame(frame=frame)
@@ -293,7 +292,7 @@ def parse_graph_stream(
 def graphs_from_jelly(
     inp: IO[bytes],
     store: Store | str = "default",
-) -> Generator[Any] | Generator[Dataset] | Generator[Graph]:
+) -> Generator[Any]:
     """
     Take jelly file and return generators based on the detected logical type.
 
@@ -317,15 +316,15 @@ def graphs_from_jelly(
     options, frames = get_options_and_frames(inp)
 
     if options.stream_types.logical_type == jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES:
-        yield parse_flat_triples_stream(frames=frames, options=options, store=store)
+        yield parse_flat_triples_stream(frames=frames, options=options, store=store) # type: ignore[call-arg]
         return
 
     if options.stream_types.logical_type == jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS:
-        yield parse_flat_quads_stream(frames=frames, options=options, store=store)
+        yield parse_flat_quads_stream(frames=frames, options=options, store=store) # type: ignore[call-arg]
         return
 
     if options.stream_types.logical_type == jelly.LOGICAL_STREAM_TYPE_GRAPHS:
-        yield from parse_graph_stream(frames=frames, options=options, store=store)
+        yield from parse_graph_stream(frames=frames, options=options, store=store) # type: ignore[call-arg]
         return
 
     logical_type_name = jelly.LogicalStreamType.Name(options.stream_types.logical_type)
@@ -367,11 +366,11 @@ def parse_jelly_flat(
     if options.stream_types.physical_type in (jelly.PHYSICAL_STREAM_TYPE_QUADS,
                                               jelly.PHYSICAL_STREAM_TYPE_GRAPHS):
         # a hack to bypass RDFLib wrapping dataset into graph before calling parse
-        sink = Dataset(sink.store)
+        dataset = Dataset(sink.store)
         parse_flat_quads_stream(
             frames=frames,
             options=options,
-            dataset=sink
+            dataset=dataset # type: ignore[arg-type]
         )
         return
     physical_type_name = jelly.PhysicalStreamType.Name(
@@ -383,7 +382,7 @@ def parse_jelly_flat(
 
 class RDFLibJellyParser(RDFLibParser):
 
-    def parse(self, source: InputSource, sink: StatementSink) -> None:
+    def parse(self, source: InputSource, sink: StatementSink) -> None:  # type: ignore[override]
         """
         Parse jelly file into provided RDFLib Graph/Dataset.
 
--- pyjelly/parse/statement_sink.py
+++ pyjelly/parse/statement_sink.py
@@ -4,3 +4,9 @@ from typing import Any, Protocol
 class StatementSink(Protocol):
     def add(self, statement: Any) -> None: ...
 
+    def bind(self, prefix: str, namespace: Any) -> None:
+        ...
+
+    @property
+    def store(self) -> Any:
+        ...
