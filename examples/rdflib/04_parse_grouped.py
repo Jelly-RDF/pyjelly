aebd5f6f69cc92806d0e9793d56299c3681b6e28
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-07-16 15:35:23 +0200

    fix

--- examples/rdflib/04_parse_grouped.py
+++ examples/rdflib/04_parse_grouped.py
@@ -16,5 +16,5 @@ with (
     for i, graph in enumerate(graphs):
         print(f"Graph {i} in the stream has {len(graph)} triples")
         # Limit to 50 graphs for demonstration -- the rest will not be parsed
-        if i >= 100:
+        if i >= 50:
             break
