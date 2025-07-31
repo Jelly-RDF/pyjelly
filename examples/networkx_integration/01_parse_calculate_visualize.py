408b6860f750ef7f0b7de0738d56fc4825de8e07
JA-GK-00 <aaf6@o2.pl>
2025-07-31 17:20:18 +0200

    Formatting

--- examples/networkx_integration/01_parse_calculate_visualize.py
+++ examples/networkx_integration/01_parse_calculate_visualize.py
@@ -9,9 +9,10 @@ url = "https://w3id.org/riverbench/datasets/politiquices/1.0.3/files/jelly_10K.j
 
 # Load example jelly file
 with urllib.request.urlopen(url) as r:
-        fd, example_file = tempfile.mkstemp(suffix=".jelly"); os.close(fd)
-        with gzip.GzipFile(fileobj=r) as g, open(example_file, "wb") as out:
-            shutil.copyfileobj(g, out)
+    fd, example_file = tempfile.mkstemp(suffix=".jelly")
+    os.close(fd)
+    with gzip.GzipFile(fileobj=r) as g, open(example_file, "wb") as out:
+        shutil.copyfileobj(g, out)
 
 # Parse RDF from the Jelly format
 rdf_g = Graph()
@@ -31,7 +32,7 @@ print("Top 5 nodes sorted by degree:")
 for node, deg in top5:
     print(f"{node}: {deg}")
 
-#Helper function
+# Helper function
 norm = lambda x: str(x).strip().lower()
 
 # Example calculation, shortest path between two nodes (provided at least two nodes)
@@ -50,9 +51,7 @@ pos_sub = nx.spring_layout(subg, k=5, iterations=100, scale=4)
 plt.figure(figsize=(10, 10))
 
 # Introduce your own settings for display
-nx.draw_networkx(
-    subg, pos_sub, font_size=12, node_size=250, linewidths=0.7
-)
+nx.draw_networkx(subg, pos_sub, font_size=12, node_size=250, linewidths=0.7)
 plt.axis("off")
 plt.show()
 
