41585d2bfe00ec09ceef500756148af9afd8e3f1
JA-GK-00 <aaf6@o2.pl>
2025-08-05 18:51:05 +0200

    Forgot file

--- tests/unit_tests/test_serialize/test_lookups/test_term_encoder.py
+++ tests/unit_tests/test_serialize/test_lookups/test_term_encoder.py
@@ -13,10 +13,9 @@ from pyjelly.serialize.encode import (
 )
 
 
-def test_split_iri_no_sep() -> None:
-    prefix, name = split_iri("example_A")
-    assert prefix == ""
-    assert name == "example_A"
+def test_split_iri_invalid_iri_raises() -> None:
+    with pytest.raises(ValueError, match=r"Invalid IRI: example_A"):
+        split_iri("example_A")
 
 
 def test_encode_literal_fails_with_disabled_datatype_lookup() -> None:
