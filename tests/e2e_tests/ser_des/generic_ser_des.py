e1164aa7b78e98ea268fb26600f403b23794726e
JA-GK-00 <aaf6@o2.pl>
2025-08-13 15:36:57 +0200

    Now they actually work

--- tests/e2e_tests/ser_des/generic_ser_des.py
+++ tests/e2e_tests/ser_des/generic_ser_des.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import io
+from pyjelly import jelly
 
 from pyjelly.integrations.generic.generic_sink import (
     GenericStatementSink,
+    Prefix,
 )
-from pyjelly.integrations.generic.parse import parse_jelly_to_graph
-from pyjelly.integrations.generic.serialize import grouped_stream_to_file, flat_stream_to_file
+from pyjelly.integrations.generic.parse import parse_jelly_flat
+from pyjelly.parse.ioutils import get_options_and_frames
+from pyjelly.integrations.generic.serialize import flat_stream_to_file
 from pyjelly.options import LookupPreset
 from pyjelly.serialize.flows import FlatQuadsFrameFlow, FlatTriplesFrameFlow
 from pyjelly.serialize.streams import SerializerOptions
@@ -17,11 +20,20 @@ class GenericSerDes:
         self.name = "generic"
 
     def read_quads(self, in_bytes: bytes) -> GenericStatementSink:
-        return parse_jelly_to_graph(io.BytesIO(in_bytes))
+        bio = io.BytesIO(in_bytes)
+        options, frames = get_options_and_frames(bio)
+        sink = GenericStatementSink()
+        for item in parse_jelly_flat(inp=bio, frames=frames, options=options):
+            if isinstance(item, Prefix):
+                sink.bind(item.prefix, item.iri)
+            else:
+                sink.add(item)
+        return sink
 
     def write_quads(self, in_graph: GenericStatementSink) -> bytes:
         out = io.BytesIO()
-        flat_stream_to_file(in_graph.store, out)
+        options = SerializerOptions(logical_type=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS)
+        flat_stream_to_file(in_graph.store, out, options=options)
         return out.getvalue()
 
     def read_quads_jelly(self, in_bytes: bytes) -> GenericStatementSink:
@@ -32,17 +44,28 @@ class GenericSerDes:
     ) -> bytes:
         out = io.BytesIO()
         options = SerializerOptions(
-            flow=FlatQuadsFrameFlow(frame_size=frame_size), lookup_preset=preset
+            flow=FlatQuadsFrameFlow(frame_size=frame_size),
+            logical_type=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
+            lookup_preset=preset,
         )
-        grouped_stream_to_file((g for g in [in_graph]), out, options=options)
+        flat_stream_to_file(in_graph.store, out, options=options)
         return out.getvalue()
 
     def read_triples(self, in_bytes: bytes) -> GenericStatementSink:
-        return parse_jelly_to_graph(io.BytesIO(in_bytes))
+        bio = io.BytesIO(in_bytes)
+        options, frames = get_options_and_frames(bio)
+        sink = GenericStatementSink()
+        for item in parse_jelly_flat(inp=bio, frames=frames, options=options):
+            if isinstance(item, Prefix):
+                sink.bind(item.prefix, item.iri)
+            else:
+                sink.add(item)
+        return sink
 
     def write_triples(self, in_graph: GenericStatementSink) -> bytes:
         out = io.BytesIO()
-        flat_stream_to_file(in_graph.store, out)
+        options = SerializerOptions(logical_type=jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES)
+        flat_stream_to_file(in_graph.store, out, options=options)
         return out.getvalue()
 
     def read_triples_jelly(self, in_bytes: bytes) -> GenericStatementSink:
@@ -53,7 +76,9 @@ class GenericSerDes:
     ) -> bytes:
         out = io.BytesIO()
         options = SerializerOptions(
-            flow=FlatTriplesFrameFlow(frame_size=frame_size), lookup_preset=preset
+            flow=FlatTriplesFrameFlow(frame_size=frame_size),
+            logical_type=jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES,
+            lookup_preset=preset,
         )
-        grouped_stream_to_file((g for g in [in_graph]), out, options=options)
+        flat_stream_to_file(in_graph.store, out, options=options)
         return out.getvalue()
