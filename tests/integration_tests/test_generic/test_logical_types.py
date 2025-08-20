337c39eb9574d9378d870914cd7d7aa454b71bee
kapystya <lapkinvladimir12@gmail.com>
2025-08-20 20:16:03 +0200

    Add extra tests

--- tests/integration_tests/test_generic/test_logical_types.py
+++ tests/integration_tests/test_generic/test_logical_types.py
@@ -25,6 +25,7 @@ from pyjelly.integrations.generic.parse import (
 )
 from pyjelly.integrations.generic.serialize import (
     flat_stream_to_file,
+    graphs_stream_frames,
     grouped_stream_to_file,
 )
 from pyjelly.parse.ioutils import get_options_and_frames
@@ -60,6 +61,48 @@ def _make_flat_quads_bytes() -> bytes:
 
     output = io.BytesIO()
     flat_stream_to_file(sink.store, output)
+    return output.getvalue()
+
+
+def _make_physical_graphs_bytes() -> bytes:
+    from pyjelly.integrations.generic.serialize import GenericSinkTermEncoder
+    from pyjelly.serialize.streams import GraphStream
+
+    options = SerializerOptions(
+        logical_type=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
+    )
+
+    stream = GraphStream(
+        encoder=GenericSinkTermEncoder(),
+        options=options,
+    )
+
+    sink = GenericStatementSink()
+
+    sink.add(
+        Quad(
+            IRI("http://ex/s1"),
+            IRI("http://ex/p1"),
+            IRI("http://ex/o1"),
+            IRI("http://ex/g1"),
+        )
+    )
+    sink.add(
+        Quad(
+            IRI("http://ex/s2"),
+            IRI("http://ex/p2"),
+            IRI("http://ex/o2"),
+            IRI("http://ex/g2"),
+        )
+    )
+
+    output = io.BytesIO()
+
+    frames = list(graphs_stream_frames(stream, sink))
+
+    for frame in frames:
+        output.write(frame.SerializeToString())
+
     return output.getvalue()
 
 
@@ -189,6 +232,42 @@ class Case:
             make_bytes=_make_grouped_graphs_bytes,
             expected_physical=jelly.PHYSICAL_STREAM_TYPE_TRIPLES,
             expected_logical=jelly.LOGICAL_STREAM_TYPE_GRAPHS,
+            parser=generic_parse_jelly_grouped,
+            strict=False,
+            should_raise=False,
+            msg=None,
+        ),
+        Case(
+            make_bytes=_make_physical_graphs_bytes,
+            expected_physical=jelly.PHYSICAL_STREAM_TYPE_GRAPHS,
+            expected_logical=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
+            parser=generic_parse_jelly_flat,
+            strict=True,
+            should_raise=False,
+            msg=None,
+        ),
+        Case(
+            make_bytes=_make_physical_graphs_bytes,
+            expected_physical=jelly.PHYSICAL_STREAM_TYPE_GRAPHS,
+            expected_logical=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
+            parser=generic_parse_jelly_flat,
+            strict=False,
+            should_raise=False,
+            msg=None,
+        ),
+        Case(
+            make_bytes=_make_physical_graphs_bytes,
+            expected_physical=jelly.PHYSICAL_STREAM_TYPE_GRAPHS,
+            expected_logical=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
+            parser=generic_parse_jelly_grouped,
+            strict=True,
+            should_raise=True,
+            msg="expected GROUPED",
+        ),
+        Case(
+            make_bytes=_make_physical_graphs_bytes,
+            expected_physical=jelly.PHYSICAL_STREAM_TYPE_GRAPHS,
+            expected_logical=jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS,
             parser=generic_parse_jelly_grouped,
             strict=False,
             should_raise=False,
