5fbcc844da52d8bbdc54bcc766b3dde8811f6c40
kapystya <lapkinvladimir12@gmail.com>
2025-08-20 14:46:07 +0200

    Add tests, make true in generic

--- tests/integration_tests/test_generic/test_serializer_features.py
+++ tests/integration_tests/test_generic/test_serializer_features.py
@@ -1,48 +1,57 @@
+from typing import Any, Final, cast
+
 import pytest
+
 from pyjelly import jelly
-from pyjelly.options import StreamParameters
 from pyjelly.integrations.generic.serialize import guess_options, guess_stream
-from pyjelly.integrations.generic.streams import SerializerOptions
+from pyjelly.options import StreamParameters
+from pyjelly.serialize.streams import SerializerOptions
 
 
 class _Sink:
-    def __init__(self, is_triples_sink: bool) -> None:
-        self.is_triples_sink = is_triples_sink
+    def __init__(self, *, is_triples_sink: bool) -> None:
+        self.is_triples_sink: Final[bool] = is_triples_sink
 
 
 @pytest.mark.parametrize(
-    "sink,expected_logical",
+    ("sink", "expected_logical"),
     [
-        (_Sink(True), jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
-        (_Sink(False), jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
+        (_Sink(is_triples_sink=True), jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
+        (_Sink(is_triples_sink=False), jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
     ],
     ids=["triples-sink", "quads-sink"],
 )
-def test_defaults_generic(sink, expected_logical):
-    opts = guess_options(sink)
+def test_defaults_generic(
+    sink: Any,
+    expected_logical: int,
+) -> None:
+    opts = guess_options(cast(Any, sink))
     assert opts.logical_type == expected_logical
+    # generic supports both by default
     assert opts.params.rdf_star is True
     assert opts.params.generalized_statements is True
 
 
 @pytest.mark.parametrize(
-    "sink,logical",
+    ("sink", "logical"),
     [
-        (_Sink(True), jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
-        (_Sink(False), jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
+        (_Sink(is_triples_sink=True), jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
+        (_Sink(is_triples_sink=False), jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
     ],
     ids=["triples-override", "quads-override"],
 )
-def test_override_generic(sink, logical):
+def test_override_generic(
+    sink: Any,
+    logical: int,
+) -> None:
     user_opts = SerializerOptions(
-        logical_type=logical,
+        logical_type=cast(jelly.LogicalStreamType, logical),
         params=StreamParameters(
             rdf_star=False,
             generalized_statements=False,
         ),
     )
-    stream = guess_stream(user_opts, sink)
-    so = stream.stream_options()
-    assert so.stream_types.logical_type == logical
-    assert so.params.rdf_star is False
-    assert so.params.generalized_statements is False
+    stream = guess_stream(user_opts, cast(Any, sink))
+    assert stream.stream_types.logical_type == logical
+    assert stream.options.params.rdf_star is False
+    assert stream.options.params.generalized_statements is False
--- tests/integration_tests/test_rdflib/test_serializer_features.py
+++ tests/integration_tests/test_rdflib/test_serializer_features.py
@@ -1,22 +1,29 @@
+from typing import Callable, Union, cast
+
 import pytest
 from rdflib import Graph
 from rdflib.graph import Dataset
 
 from pyjelly import jelly
-from pyjelly.options import StreamParameters
 from pyjelly.integrations.rdflib.serialize import guess_options, guess_stream
-from pyjelly.integrations.rdflib.streams import SerializerOptions
+from pyjelly.options import StreamParameters
+from pyjelly.serialize.streams import SerializerOptions
+
+Store = Union[Graph, Dataset]
 
 
 @pytest.mark.parametrize(
-    "make_store,expected_logical",
+    ("make_store", "expected_logical"),
     [
         (Graph, jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
         (Dataset, jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
     ],
     ids=["graph->triples", "dataset->quads"],
 )
-def test_defaults_rdflib(make_store, expected_logical):
+def test_defaults_rdflib(
+    make_store: Callable[[], Store],
+    expected_logical: int,
+) -> None:
     store = make_store()
     opts = guess_options(store)
     assert opts.logical_type == expected_logical
@@ -25,24 +32,27 @@ def test_defaults_rdflib(make_store, expected_logical):
 
 
 @pytest.mark.parametrize(
-    "make_store,logical",
+    ("make_store", "logical"),
     [
         (Graph, jelly.LOGICAL_STREAM_TYPE_FLAT_TRIPLES),
         (Dataset, jelly.LOGICAL_STREAM_TYPE_FLAT_QUADS),
     ],
     ids=["graph-override", "dataset-override"],
 )
-def test_override_rdflib(make_store, logical):
+def test_override_rdflib(
+    make_store: Callable[[], Store],
+    logical: int,
+) -> None:
     store = make_store()
     user_opts = SerializerOptions(
-        logical_type=logical,
+        logical_type=cast(jelly.LogicalStreamType, logical),
         params=StreamParameters(
             rdf_star=True,
             generalized_statements=True,
         ),
     )
     stream = guess_stream(user_opts, store)
-    so = stream.stream_options()
-    assert so.stream_types.logical_type == logical
-    assert so.params.rdf_star is True
-    assert so.params.generalized_statements is True
+    # stream_options() returns None; inspect fields directly
+    assert stream.stream_types.logical_type == logical
+    assert stream.options.params.rdf_star is True
+    assert stream.options.params.generalized_statements is True
