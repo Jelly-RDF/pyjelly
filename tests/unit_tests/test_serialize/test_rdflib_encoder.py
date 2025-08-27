ff052873305ddd86036c5021f45acda25d5525da
Anastasiya Danilenka <anastasiya.danilenka.dokt@pw.edu.pl>
2025-08-27 18:07:19 +0200

    added test for unsupported quoted triples in rdflib encode_spo

--- tests/unit_tests/test_serialize/test_rdflib_encoder.py
+++ tests/unit_tests/test_serialize/test_rdflib_encoder.py
@@ -4,6 +4,7 @@ from rdflib import URIRef
 from pyjelly import jelly
 from pyjelly.integrations.rdflib.serialize import RDFLibTermEncoder
 from pyjelly.options import LookupPreset
+from pyjelly.serialize.encode import Slot
 
 
 def test_graph_not_implemented() -> None:
@@ -11,5 +12,15 @@ def test_graph_not_implemented() -> None:
     with pytest.raises(NotImplementedError, match="unsupported term type"):
         enc.encode_graph(
             (URIRef("http://ex/s1"), URIRef("http://ex/p1"), URIRef("http://ex/o1")),
+            jelly.RdfQuad(),
+        )
+
+
+def test_spo_not_implemented() -> None:
+    enc = RDFLibTermEncoder(lookup_preset=LookupPreset())
+    with pytest.raises(NotImplementedError, match="unsupported term type"):
+        enc.encode_spo(
+            (URIRef("http://ex/s1"), URIRef("http://ex/p1"), URIRef("http://ex/o1")),
+            Slot.subject,
             jelly.RdfQuad(),
         )
