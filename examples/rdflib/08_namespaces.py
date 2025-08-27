1f31a12d1f69beefb7f6ff72d4f1649a23068c6c
kapystya <lapkinvladimir12@gmail.com>
2025-08-27 16:01:45 +0200

    change formatting

--- examples/rdflib/08_namespaces.py
+++ examples/rdflib/08_namespaces.py
@@ -1,7 +1,7 @@
 from rdflib import Graph, Namespace, URIRef, Literal
 from pyjelly.integrations.rdflib.serialize import SerializerOptions, StreamParameters
 
-# build a tiny graph and bind a prefix
+# Build a tiny graph and bind a prefix
 g = Graph()
 EX = Namespace("http://example.org/")
 g.namespace_manager.bind("ex", EX)
@@ -9,17 +9,17 @@ g.add((EX.alice, URIRef("http://xmlns.com/foaf/0.1/name"), Literal("Alice")))
 
 print("IN  namespaces:", dict(g.namespaces()))
 
-# enable namespace declarations in jelly output
+# Enable namespace declarations in Jelly output
 options = SerializerOptions(
     params=StreamParameters(
-        generalized_statements=False, rdf_star=False, namespace_declarations=True
+        namespace_declarations=True
     )
 )
 
-# serialize with options
+# Serialize with options
 g.serialize("sample_test.jelly", format="jelly", options=options)
 
-# parse back and check namespaces
+# Parse back and check namespaces
 g_new = Graph()
 g_new.parse("sample_test.jelly", format="jelly")
 print("OUT namespaces:", dict(g_new.namespaces()))
