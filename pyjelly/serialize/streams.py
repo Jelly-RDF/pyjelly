112fa13515431400335acf3f1c012cb8a675e95a
kapystya <lapkinvladimir12@gmail.com>
2025-12-04 20:47:29 +0100

    deleted #noqa for i001

--- pyjelly/serialize/streams.py
+++ pyjelly/serialize/streams.py
@@ -146,7 +146,7 @@ class Stream:
         if cls is Stream:
             msg = "Stream is an abstract base class, use a subclass instead"
             raise TypeError(msg)
-        from pyjelly.integrations.rdflib.serialize import (  # noqa: I001, PLC0415
+        from pyjelly.integrations.rdflib.serialize import (  # noqa: PLC0415
             RDFLibTermEncoder,
         )
 
